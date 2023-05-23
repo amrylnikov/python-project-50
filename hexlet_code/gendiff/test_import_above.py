1da5aae1ea710bb9a31773a557568c7486987b5c
amrylnikov <amrylnikov@gmail.com>
2023-05-23 13:17:52 +0300

    deleted wront test files

--- hexlet_code/gendiff/test_import_above.py
+++
@@ -1,9 +0,0 @@
-from main import generate_diff
-import json
-
-deep_json1 = r'C:\Users\Алексей\PycharmProjects\diff-calc\tests\fixtures\file1.json'
-deep_json2 = r'C:\Users\Алексей\PycharmProjects\diff-calc\tests\fixtures\file2.json'
-t = json.load(open(deep_json1))
-h = json.load(open(deep_json2))
-diff = generate_diff(t, h)
-print(diff)
